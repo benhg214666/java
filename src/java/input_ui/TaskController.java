72d43e9e222abf0ec3b9573847913552ae079138
shiuan <chiou20000208@gmail.com>
2026-06-14 20:06:25 +0800

    Connect task input view events in controller

--- src/java/input_ui/TaskController.java
+++ src/java/input_ui/TaskController.java
@@ -2,23 +2,36 @@ import java.util.List;
 
 public class TaskController {
 
-    private TaskInputView taskInputView;
+    private TaskInputView view;
 
-    public TaskController(TaskInputView taskInputView) {
-        this.taskInputView = taskInputView;
+    public TaskController(TaskInputView view) {
+        this.view = view;
         registerEventListeners();
     }
 
     private void registerEventListeners() {
+        view.onAddButtonClicked(this::handleAddButtonClick);
+        view.onDeleteButtonClicked(this::handleDeleteButtonClick);
+        view.onExportButtonClicked(this::handleExportButtonClick);
     }
 
     private void handleAddButtonClick() {
+        view.addEmptyRow();
     }
 
     private void handleDeleteButtonClick() {
+        int selectedRowIndex = view.getSelectedRowIndex();
+
+        if (isValidRowIndex(selectedRowIndex)) {
+            view.deleteRow(selectedRowIndex);
+            return;
+        }
+
+        view.showErrorMessage("Please select a row to delete.");
     }
 
     private void handleExportButtonClick() {
+        view.showSuccessMessage("Export JSON will be implemented in the next phase.");
     }
 
     List<Task> collectTasksFromView() {
@@ -27,5 +40,9 @@ public class TaskController {
 
     String convertTasksToJson(List<Task> tasks) {
         return null;
+    }
+
+    private boolean isValidRowIndex(int rowIndex) {
+        return rowIndex >= 0 && rowIndex < view.getTaskRowCount();
     }
 }
